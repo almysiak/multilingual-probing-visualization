f9a183e48e0c17f32df434b836c105c5e2174438
Aleksandra Mysiak <amysiak@asusgpu0.mimuw.edu.pl>
2023-10-11 21:08:27 +0200

    new configs

--- run_missing_configs.sh
+++ run_missing_configs.sh
@@ -1,6 +1,6 @@
-for SEED in 1
+for SEED in 0
 do
-    for file in configs/missing_slavic/*.yaml
+    for file in configs/all_new/*.yaml
     do
         srun --partition=common --qos=2gpu1d --gres=gpu:1 python probing/run_experiment.py $file --train-probe=1 --seed=$SEED
     done
